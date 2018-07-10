882b8518fcffb98c1db51b896ebe38acc9c9f045
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2018-07-10 14:12:04 -0300

    Continuação site esporte

---
+++ esporte/corel/sitesport/esporte5_arquivos/b.js
@@ -0,0 +1,2 @@
+window['_ttq_tt_uol'].push(['_setTTProfile', 'E:1|S:41.68.1524222000,1.5.1524229200,50.5.1525132800|A:3|X:3|G:1|T:3|C:1_sao%20lourenco_minas%20gerais_br_1526646518459_2852178946']);
+window['_ttq_tt_uol'].push(['_setProfile','58,68,72,24,52,34,38,20']);
---
+++ esporte/corel/sitesport/esporte5_arquivos/profile.js
@@ -0,0 +1,4 @@
+_ttprofiles.push(['_setTTProfile', 'E:1|S:41.68.1524222000,1.5.1524229200,50.5.1525132800|A:3|X:3|G:1|T:3|C:1_sao%20lourenco_minas%20gerais_br_1526646518459_2852178946']);
+_ttprofiles.push(['_setProfile','58,68,72,24,52,34,38,20']);
+
+_ttprofiles.push(['_setCA','CA9882,CA7245,CA11754,CA11405,CA9881,CA7240,CA11396,CA16166_1526647613']);
